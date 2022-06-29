26db5d0554f763296b2eabe15c20de5f4afe173c
Hemanth Reddy Loka <lokahemanthreddy@gmail.com>
2022-06-29 01:41:44 -0500

    created routing in the customer-management app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+// switch will make sure that only one component is rendered at a time
+// when ever we want to render we go inside browserRouter
 import ListCustomerComponent from './components/ListCustomerComponent'
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
+
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListCustomerComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Routes>
+                <Route path = "/" element = {<ListCustomerComponent/>}></Route>
+                <Route path = "/customers" element = {<ListCustomerComponent/>}></Route>
+            </Routes>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
